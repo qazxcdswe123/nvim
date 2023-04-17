0c0ae187fe1beb14c971cd211b5c7d15ca6dbbf5
Neil-Chen <v009008777@hotmail.com>
2023-04-17 15:20:01 +0800

    before toggleterm

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,14 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-local function force_map(mode, lhs, rhs, opts)
-  local options = { noremap = true, silent = true }
-  if opts then
-    options = vim.tbl_extend("force", options, opts)
-  end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
-
 local function map(mode, lhs, rhs, opts)
   local keys = require("lazy.core.handler").handlers.keys
   ---@cast keys LazyKeysHandler
@@ -21,5 +13,4 @@ local function map(mode, lhs, rhs, opts)
 end
 
 map({ "n", "v" }, "J", "5j", { silent = true })
--- vim.keymap.del("n", "K")
 map({ "n", "v" }, "K", "5k", { silent = true })
