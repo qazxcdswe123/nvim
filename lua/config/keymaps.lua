3710c07418ac28ba8a9a3dff518f5fad3a83b235
Neil-Chen <v009008777@hotmail.com>
2023-05-14 23:04:26 +0800

    term fix

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -34,18 +34,3 @@ map(nvt, "<C-h>", require("smart-splits").move_cursor_left)
 map(nvt, "<C-j>", require("smart-splits").move_cursor_down)
 map(nvt, "<C-k>", require("smart-splits").move_cursor_up)
 map(nvt, "<C-l>", require("smart-splits").move_cursor_right)
-
--- function _G.set_terminal_keymaps()
---   local opts = { buffer = 0 }
---   local map = vim.keymap.set
---   map("t", "<esc>", [[<C-\><C-n>]], opts)
---   map("t", "jk", [[<C-\><C-n>]], opts)
---   map("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
---   map("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
---   map("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
---   map("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
---   map("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
--- end
-
--- if you only want these mappings for toggle term use term://*toggleterm#* instead
-vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
