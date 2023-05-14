9e063f675693f5670cbe34fc1ae068a67dbc6b5c
Neil-Chen <v009008777@hotmail.com>
2023-05-14 23:03:06 +0800

    open current file explorer

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,5 +1,14 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  keys = {
+    {
+      "<leader>fE",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = vim.fn.expand("%:p:h") })
+      end,
+      desc = "Explorer NeoTree (cwd)",
+    },
+  },
   opts = {
     filesystem = {
       group_empty_dirs = true,
