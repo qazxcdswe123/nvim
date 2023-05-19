d6bc3998963c856d1db338731d5248afa2e63292
Neil-Chen <v009008777@hotmail.com>
2023-05-20 07:49:07 +0800

    mini rust lifetime

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -8,4 +8,14 @@ return {
       { "<leader>bX", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
     },
   },
+
+  -- autoclose pairs
+  {
+    "echasnovski/mini.pairs",
+    opts = {
+      mappings = {
+        ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a&\\].", register = { cr = false } },
+      },
+    },
+  },
 }
