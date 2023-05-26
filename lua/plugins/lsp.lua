ae1d0a5f49905c4055de720cb932b1391c75a671
Neil-Chen <v009008777@hotmail.com>
2023-05-26 17:06:29 +0800

    rm overseer

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,6 +11,7 @@ return {
         opts = { lsp = { auto_attach = true } },
       },
     },
+    -- keymap start
     init = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       -- change a keymap
@@ -20,6 +21,7 @@ return {
       -- add a keymap
       keys[#keys + 1] = { "<leader>k", vim.lsp.buf.hover, desc = "Hover" }
     end,
+    -- keymap end
     opts = {
       autoformat = false,
     },
--- lua/plugins/overseer.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "stevearc/overseer.nvim",
-  config = true,
-  cmd = { "OverseerRun", "OverseerToggle" },
-  keys = {
-    { "<leader>co", "<cmd>OverseerRun<CR>", desc = "overseer open" },
-    { "<leader>cO", "<cmd>OverseerToggle<CR>", desc = "overseer toggle" },
-  },
-}
