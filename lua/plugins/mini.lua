117f329fd9850841bb692b3bc5824360c465b181
Neil-Chen <v009008777@hotmail.com>
2023-06-21 09:02:57 +0800

    rm surround keymap

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -19,18 +19,18 @@ return {
     },
   },
 
-  {
-    "echasnovski/mini.surround",
-    opts = {
-      mappings = {
-        add = "gsa",
-        delete = "gsd",
-        find = "gsf",
-        find_left = "gsF",
-        highlight = "gsh",
-        replace = "gsr",
-        update_n_lines = "gsn",
-      },
-    },
-  },
+  -- {
+  --   "echasnovski/mini.surround",
+  --   opts = {
+  --     mappings = {
+  --       add = "gsa",
+  --       delete = "gsd",
+  --       find = "gsf",
+  --       find_left = "gsF",
+  --       highlight = "gsh",
+  --       replace = "gsr",
+  --       update_n_lines = "gsn",
+  --     },
+  --   },
+  -- },
 }
