b41fc214f712118238705b4152551c3600f290d1
Neil-Chen <v009008777@hotmail.com>
2023-07-14 13:20:45 +0800

    dash keybind + lightbulb

--- lua/plugins/dash.lua
+++ lua/plugins/dash.lua
@@ -3,8 +3,8 @@ return {
     'mrjones2014/dash.nvim',
     build = "make install",
     keys = {
-      { '<leader>fd', '<cmd>Dash<CR>', desc = "Dash Search" },
-      { '<leader>fD', '<cmd>DashWord<CR>', desc = "Dash Search Word" },
+      { '<leader>fD', '<cmd>Dash<CR>', desc = "Dash Search" },
+      { '<leader>fd', '<cmd>DashWord<CR>', desc = "Dash Search Word" },
     }
   }
 }
